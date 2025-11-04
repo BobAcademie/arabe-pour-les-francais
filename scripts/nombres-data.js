5e257463057d64db7cd04e2b582ff26eeabe7917
BobAcademie <aboubakrhadid17@gmail.com>
2025-11-04 14:53:27 +0100

    Update nombres-data.js

--- scripts/nombres-data.js
+++ scripts/nombres-data.js
@@ -1,7 +1,7 @@
 // Questions spécifiques à la leçon des jours de la semaine
 const quizData = [
   {
-    question: "اختر العدد المناسب للصورة:",
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
     image: "../images/vocabulaire/1.JPG",
     a: "واحد",
     b: "ثلاثة",
@@ -11,7 +11,7 @@ const quizData = [
   },
 
   {
-    question: "اختر العدد المناسب للصورة:",
+    question: "  ما هو العدد الذي يظهر في الصورة؟ :",
     image: "../images/vocabulaire/4.JPG",
     a: "واحد",
     b: "ثلاثة",
@@ -20,7 +20,7 @@ const quizData = [
     correct: "c",
   },
   {
-    question: "اختر العدد المناسب للصورة:",
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
     image: "../images/vocabulaire/5.JPG",
     a: "واحد",
     b: "ثلاثة",
@@ -28,7 +28,57 @@ const quizData = [
     d: "خمسة",
     correct: "d",
   },
+  
+  {
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
+    image: "../images/vocabulaire/3.JPG",
+    a: "واحد",
+    b: "ثلاثة",
+    c: "اربعة",
+    d: "خمسة",
+    correct: "b",
+  }
+  ,
+  {
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
+    image: "../images/vocabulaire/2.JPG",
+    a: "واحد",
+    b: "اِثْنَانْ",
+    c: "أَرْبَعَة",
+    d: "خمسة",
+    correct: "b",
+  },
+  ,
+  {
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
+    image: "../images/vocabulaire/7.JPG",
+    a: "واحد",
+    b: "ثلاثة",
+    c: "سَبْعَة",
+    d: "أَرْبَعَة",
+    correct: "c",
+  },
+   {
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
+    image: "../images/vocabulaire/9.JPG",
+    a: "عَشَرَة",
+    b: "ثَلاثَة",
+    c: "سَبْعَة",
+    d: "تِسْعَة",
+    correct: "d",
+  },
+  {
+    question: "   ما هو العدد الذي يظهر في الصورة؟",
+    image: "../images/vocabulaire/6.JPG",
+    a: "سِتَّة",
+    b: "ثَلاثَة",
+    c: "سَبْعَة",
+    d: "تِسْعَة",
+    correct: "a",
+  },
+  
 ];
+
 
 
 
