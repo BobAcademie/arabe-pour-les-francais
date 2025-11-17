8f6425e9bbe584167613fac0131e9b9193a82fee
BobAcademie <aboubakrhadid17@gmail.com>
2025-11-17 12:55:57 +0100

    Update quiz-data.js

--- scripts/quiz-data.js
+++ scripts/quiz-data.js
@@ -29,6 +29,54 @@ const quizzes = [
   ],
   [
     {
+      arabe: "الثلاثاء",
+      mot: "mardi",
+      image: "../images/vocabulaire/jours/mardi.JPG",
+      son: "../audio/jours/mardi.MP3",
+    },
+
+    {
+      arabe: "السبت",
+      mot: "samedi",
+      image: "../images/vocabulaire/jours/samedi.JPG",
+      son: "../audio/jours/samedi.MP3",
+    },
+    {
+      arabe: "الخميس",
+      mot: "jeudi",
+      image: "../images/vocabulaire/jours/jeudi.JPG",
+      son: "../audio/jours/jeudi.MP3",
+    },
+    {
+      arabe: "الأربعاء",
+      mot: "mercredi",
+      image: "../images/vocabulaire/jours/mercredi.JPG",
+      son: "../audio/jours/mercredi.MP3",
+    },
+    
+  ],
+  [
+    {
+      arabe: "اثنان",
+      mot: "deux",
+      image: "../images/vocabulaire/2.JPG",
+      son: "../audio/nombres/2.mp3",
+    },
+    {
+      arabe: "واحد",
+      mot: "un",
+      image: "../images/vocabulaire/1.JPG",
+      son: "../audio/nombres/1.mp3",
+    },
+    {
+      arabe: "ثلاثة",
+      mot: "trois",
+      image: "../images/vocabulaire/3.JPG",
+      son: "../audio/jours/3.mp3",
+    },
+  ],
+  [
+    {
       arabe: "اثنان",
       mot: "deux",
       image: "../images/vocabulaire/2.JPG",
@@ -48,6 +96,7 @@ const quizzes = [
     },
   ],
 ];
+
 
 
 
