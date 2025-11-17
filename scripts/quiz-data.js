c2f008387ee6aaa631c802e9b6f7e1707e1ec7cb
BobAcademie <aboubakrhadid17@gmail.com>
2025-11-17 13:21:52 +0100

    Update quiz-data.js

--- scripts/quiz-data.js
+++ scripts/quiz-data.js
@@ -29,23 +29,23 @@ const quizzes = [
   ],
   [
     {
-      arabe: "الثلاثاء",
-      mot: "mardi",
-      image: "../images/vocabulaire/jours/mardi.JPG",
-      son: "../audio/jours/mardi.MP3",
+      arabe: "الجمعة",
+      mot: "vendredi",
+      image: "../images/vocabulaire/jours/vendredi.JPG",
+      son: "../audio/jours/vendredi.MP3",
     },
 
     {
-      arabe: "السبت",
-      mot: "samedi",
-      image: "../images/vocabulaire/jours/samedi.JPG",
-      son: "../audio/jours/samedi.MP3",
+      arabe: "الأحد",
+      mot: "dimanche",
+      image: "../images/vocabulaire/jours/dimanche.JPG",
+      son: "../audio/jours/dimanche.MP3",
     },
     {
-      arabe: "الخميس",
-      mot: "jeudi",
-      image: "../images/vocabulaire/jours/jeudi.JPG",
-      son: "../audio/jours/jeudi.MP3",
+      arabe: "الإثنين",
+      mot: "lundi",
+      image: "../images/vocabulaire/jours/lundi.JPG",
+      son: "../audio/jours/lundi.MP3",
     },
     {
       arabe: "الأربعاء",
@@ -72,30 +72,43 @@ const quizzes = [
       arabe: "ثلاثة",
       mot: "trois",
       image: "../images/vocabulaire/3.JPG",
-      son: "../audio/jours/3.mp3",
+      son: "../audio/nombres/3.mp3",
+    },
+    {
+      arabe: "أربعة",
+      mot: "quatre",
+      image: "../images/vocabulaire/4.JPG",
+      son: "../audio/nombres/4.mp3",
     },
   ],
   [
     {
-      arabe: "اثنان",
-      mot: "deux",
-      image: "../images/vocabulaire/2.JPG",
-      son: "../audio/nombres/2.mp3",
+      arabe: "سَبْعَة",
+      mot: "sept",
+      image: "../images/vocabulaire/7.JPG",
+      son: "../audio/nombres/7.mp3",
     },
     {
-      arabe: "واحد",
-      mot: "un",
-      image: "../images/vocabulaire/1.JPG",
-      son: "../audio/nombres/1.mp3",
+      arabe: "خَمْسَة",
+      mot: "cinq",
+      image: "../images/vocabulaire/5.JPG",
+      son: "../audio/nombres/5.mp3",
     },
     {
-      arabe: "ثلاثة",
-      mot: "trois",
-      image: "../images/vocabulaire/3.JPG",
-      son: "../audio/jours/3.mp3",
+      arabe: "تِسْعَة",
+      mot: "neuf",
+      image: "../images/vocabulaire/9.JPG",
+      son: "../audio/nombres/9.mp3",
+    },
+    {
+      arabe: "عَشَرَة",
+      mot: "dix",
+      image: "../images/vocabulaire/10.JPG",
+      son: "../audio/nombres/10.mp3",
     },
   ],
 ];
+
 
 
 
