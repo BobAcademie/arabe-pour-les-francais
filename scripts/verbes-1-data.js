6480e3db278b2d166bc65aaf960ceb5be4245052
BobAcademie <aboubakrhadid17@gmail.com>
2025-11-18 14:03:49 +0100

    Create verbes-1-data.js

---
+++ scripts/verbes-1-data.js
@@ -0,0 +1,49 @@
+const quizzes = [
+  [
+    {
+      arabe: "جَلَسَ",
+      mot: "sasoire",
+      image: "..audio/verbes1/sasoire.png",
+      son: "../audio/verbes/assis.MP3",
+    },
+
+    {
+      arabe: "وَقَفَ",
+      mot: "debout",
+      image: "..audio/verbes1/debout.png",
+      son: "../audio/verbes/debout.MP3",
+    },
+    {
+      arabe: "كَتَبَ",
+      mot: "ecrire",
+      image: "..audio/verbes1/ecrire.png",
+      son: "../audio/verbes/ecrire.MP3",
+    },
+    {
+      arabe: "أَغْلَقَ",
+      mot: "fermer",
+      image: "..audio/verbes1/fermer.png",
+      son: "../audio/verbes/fermer.MP3",
+    },
+  ],
+  [
+    {
+      arabe: "قَرَأَ",
+      mot: "lire",
+      image: "..audio/verbes1/lire.png",
+      son: "../audio/verbes/lire.MP3",
+    },
+    {
+      arabe: "فَتَحَ",
+      mot: "ouvrir",
+      image: "..audio/verbes1/ouvrir.png",
+      son: "../audio/verbes/ouvrir.MP3",
+    },
+    {
+      arabe: "جَلَسَ",
+      mot: "sasoire",
+      image: "..audio/verbes1/sasoire.png",
+      son: "../audio/verbes/assis.MP3",
+    },
+  ],
+];
