cf71045fe5cd30eafe81ba4812e621b7469083de
BobAcademie <aboubakrhadid17@gmail.com>
2025-12-09 13:16:19 +0100

    Create dialogue2-data.js

---
+++ scripts/dialogue2-data.js
@@ -0,0 +1,144 @@
+const quizData = [
+  // Correct = c
+  {
+    question: "مَنْ هَذَا؟",
+    image: "../images/dialogues/pere.PNG",
+    a: "هَذَا أَخِي",
+    b: "هَذَا جَدِّي",
+    c: "هَذَا أَبِي",
+    d: "هَذِهِ أُخْتِي",
+    correct: "c",
+    questionSound: "../audio/famille/q1.MP3",
+    sounds: {
+      a: "../audio/famille/akhi.MP3",
+      b: "../audio/famille/jaddi.MP3",
+      c: "../audio/famille/abi.MP3",
+      d: "../audio/famille/okhti.MP3",
+    },
+  },
+
+  // Correct = a
+  {
+    question: "مَنْ هَذَا؟",
+    image: "../images/dialogues/frere.PNG",
+    a: "هَذَا أَخِي",
+    b: "هَذِهِ أُمِّي",
+    c: "هَذَا أَبِي",
+    d: "هَذِهِ جَدَّتِي",
+    correct: "a",
+    questionSound: "../audio/famille/q1.MP3",
+    sounds: {
+      a: "../audio/famille/akhi.MP3",
+      b: "../audio/famille/ommi.MP3",
+      c: "../audio/famille/abi.MP3",
+      d: "../audio/famille/jaddati.MP3",
+    },
+  },
+
+  // Correct = b
+  {
+    question: "مَنْ هَذَا؟",
+    image: "../images/dialogues/grand-pere.PNG",
+    a: "هَذَا أَبِي",
+    b: "هَذَا جَدِّي",
+    c: "هَذَا أَخِي",
+    d: "هَذِهِ جَدَّتِي",
+    correct: "b",
+    questionSound: "../audio/famille/q1.MP3",
+    sounds: {
+      a: "../audio/famille/abi.MP3",
+      b: "../audio/famille/jaddi.MP3",
+      c: "../audio/famille/akhi.MP3",
+      d: "../audio/famille/jaddati.MP3",
+    },
+  },
+
+  // Correct = d
+  {
+    question: "مَنْ هَذِهِ؟",
+    image: "../images/dialogues/mere.PNG",
+    a: "هَذَا أَخِي",
+    b: "هَذَا أَبِي",
+    c: "هَذِهِ جَدَّتِي",
+    d: "هَذِهِ أُمِّي",
+    correct: "d",
+    questionSound: "../audio/famille/q2.MP3",
+    sounds: {
+      a: "../audio/famille/akhi.MP3",
+      b: "../audio/famille/abi.MP3",
+      c: "../audio/famille/jaddati.MP3",
+      d: "../audio/famille/ommi.MP3",
+    },
+  },
+
+  // Correct = b
+  {
+    question: "مَنْ هَذِهِ؟",
+    image: "../images/dialogues/grand-mere.PNG",
+    a: "هَذَا جَدِّي",
+    b: "هَذِهِ جَدَّتِي",
+    c: "هَذَا أَبِي",
+    d: "هَذِهِ أُمِّي",
+    correct: "b",
+    questionSound: "../audio/famille/q2.MP3",
+    sounds: {
+      a: "../audio/famille/jaddi.MP3",
+      b: "../audio/famille/jaddati.MP3",
+      c: "../audio/famille/abi.MP3",
+      d: "../audio/famille/ommi.MP3",
+    },
+  },
+
+  // Correct = c
+  {
+    question: "مَنْ هَذِهِ؟",
+    image: "../images/dialogues/soeur.PNG",
+    a: "هَذَا أَخِي",
+    b: "هَذِهِ أُمِّي",
+    c: "هَذِهِ أُخْتِي",
+    d: "هَذِهِ جَدَّتِي",
+    correct: "c",
+    questionSound: "../audio/famille/q2.MP3",
+    sounds: {
+      a: "../audio/famille/akhi.MP3",
+      b: "../audio/famille/ommi.MP3",
+      c: "../audio/famille/okhti.MP3",
+      d: "../audio/famille/jaddati.MP3",
+    },
+  },
+
+  // Correct = a
+  {
+    question: " le frère de mon père c'est:  ",
+    image: "",
+    a: " عَمِّي",
+    b: " جَدِّي",
+    c: " خَالي",
+    d: " أُمِّي",
+    correct: "a",
+    questionSound: "../audio/famille/q1.MP3",
+    sounds: {
+      a: "../audio/famille/3ammi1.MP3",
+      b: "../audio/famille/jaddi1.MP3",
+      c: "../audio/famille/khali1.MP3",
+      d: "../audio/famille/ommi1.MP3",
+    },
+  },
+  // Correct = c
+  {
+    question: " le frère de ma mère c'est: ",
+    image: "",
+    a: " عَمِّي",
+    b: " جَدِّي",
+    c: " خَالِي",
+    d: " أُمِّي",
+    correct: "c",
+    questionSound: "../audio/famille/q1.MP3",
+    sounds: {
+      a: "../audio/famille/3ammi1.MP3",
+      b: "../audio/famille/jaddi1.MP3",
+      c: "../audio/famille/khali1.MP3",
+      d: "../audio/famille/ommi1.MP3",
+    },
+  },
+];
